7be91c83fe4f5ae1f77774e88a4efd7c7bbfee46
hyhdb <kkminwoo@gmail.com>
2026-04-03 11:01:47 +0900

    87,88,89

--- src/main/java/com/back/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/back/global/security/CustomAuthenticationFilter.java
@@ -50,8 +50,7 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
         }
     }
 
-    private void authenticate(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+    private void authenticate(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         if(!request.getRequestURI().startsWith("/api/")) {
             filterChain.doFilter(request, response);
             return;
@@ -86,10 +85,11 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
 
         boolean isAccessTokenExists = !accessToken.isBlank();
         boolean isAccessTokenValid = false;
+        boolean isApiKeyExists = !apiKey.isBlank();
 
-        if (apiKey.isBlank()) {
-            throw new ServiceException("401-1", "apiKey가 존재하지 않습니다.");
-        }
+//        if (apiKey.isBlank()) {
+//            throw new ServiceException("401-1", "apiKey가 존재하지 않습니다.");
+//        }
 
         if (isAccessTokenExists) {
             Map<String, Object> payload = memberService.payloadOrNull(accessToken);
@@ -101,6 +101,11 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
                 member = new Member(id, username, nickname);
                 isAccessTokenValid = true;
             }
+        }
+
+        if(!isApiKeyExists) {
+            filterChain.doFilter(request, response);
+            return;
         }
 
         // accessToken으로 인증이 제대로 이루어지지 않은 경우
@@ -132,6 +137,7 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
         SecurityContextHolder
                 .getContext()
                 .setAuthentication(authentication);
+
 
         filterChain.doFilter(request, response);
     }
